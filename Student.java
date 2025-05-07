023c2b0c6b2b56145dd6cc8ffe1334a6b6304974
aratimallesh28 <ratimallesh@gmail.com>
2025-05-07 10:58:03 +0530

    save

--- Student.java
+++ Student.java
@@ -1,3 +1,23 @@
+class Courses{
+    int sem;
+    int[] marks = new int [3]; 
+    String[] courseName = new String[3];
+
+    Courses(int sem, int[] marks, String[] courseName) {
+        this.sem = sem;
+        this.marks = marks;
+        this.courseName = courseName;
+    }
+
+    void displayCourses() {
+        System.out.println("Courses offered in semester " + sem + ":");
+        for (int i =0; i < courseName.length; i++) {
+            System.out.println("Subject: "+courseName[i]+"\tMarks: " + marks[i]);
+        }
+    }
+
+}
+
 public class Student {
     String name;
     int age;
@@ -19,5 +39,8 @@ public class Student {
     public static void main(String[] args) {
         Student student = new Student("Bharat Kulkarni", 20);
         student.display();
+
+        Courses course = new Courses(2, new int[]{85, 90, 78}, new String[]{"Math", "Science", "History"});
+        course.displayCourses();
     }
 }
